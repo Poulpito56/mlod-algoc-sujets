8b9b014bdc23b6fcda70daec2ffeb5a1edb84543
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-09-26 17:32:06 +0200

    add prixTuring

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -3,57 +3,77 @@
  gcc --std=c99 -W -Wall -o prixTuring prixTuring.c
 
  Exécution
- ./prixTuring < turingWinners.txt > out.txt
+ ./prixTuring
 
  Tests
- diff out.txt turingWinners.txt
+ diff out.csv turingWinners.csv
 
- Détection de fuites mémoires
- valgrind --leak-check=yes --leak-check=full --show-leak-kinds=all --show-reachable=no ./prixTuring < turingWinners.txt > out.txt
 **/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <errno.h>
+#include <assert.h>
 
-/* This function scans a line of text (until \n) and returns a char* that contains all characters on the line (up to 255) excluding \n.
-It also ensures the \0 termination.
-**WARNING**: The result of this function has been allocated (calloc) by the function */
-char* scanLine()
+
+typedef struct {
+  int annee;
+  char *nom;
+  char *travaux;
+} Ligne;
+
+
+int numberOfWinners(FILE* f){
+	int caractere;
+	int count = 1;
+	
+	while ((caractere = fgetc(f)) != EOF) {
+    if (caractere=='\n')
+		{
+			count++;
+		}
+  }
+	return count;
+};
+
+char *readStringFromFileUntil(FILE *f, char c){
+	int caractere;
+	char *result = (char*) malloc(512*sizeof(char));
+	int i = 0;
+	while ((caractere = fgetc(f)) != EOF) {
+    if (caractere==c)
+		{
+			return result;
+		}
+		result[i]=caractere;
+		i++;
+  }
+};
+
+void readWinners(FILE *f, Ligne *ligne){
+	fscanf(f, "%i", &ligne->annee);
+	ligne->nom=readStringFromFileUntil(f, ';');
+	ligne->travaux=readStringFromFileUntil(f, ';');
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// MAIN
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int main(int argc, char** argv)
 {
-	int maxLineSize = 255;
-	char c, *line = calloc(maxLineSize+1,sizeof(char));
-
-	scanf("%250[^\n]", line);
-
-	if ( (c = getchar()) != '\n') {
-		/* we did not get all the line */
-		line[250] = '[';
-		line[251] = line[252] = line[253] = '.';
-		line[254] = ']';
-		// line[255] = '\0'; // useless because already initialized by calloc
-
-		// let's skip all chars untli the end of line
-		while (( (c = getchar()) != '\n') && c != EOF) ;
-	}
-
-	return line;
-}
-
-/* This function scans a line of text (until \n), converts it as an integer and returns this integer */
-int scanLineAsInt() {
-	int buf;
-	scanf("%i\n",&buf);
-	return buf;
-}
-
-
-int main(void)
-{
-
-	int nbGagnants = scanLineAsInt();
-	printf("nbGagnants = %i\n",nbGagnants);
-
+	char filename[] = "turingWinners.csv";
+	char outputFilename[] = "out.csv";
+	FILE* f;
+	FILE* of;
+	f = fopen(filename,"r");
+	of = fopen(outputFilename, "a");
+	
+	printf("%i", numberOfWinners(f));
+	
+	fclose(f);
+	fclose(f);
 	return EXIT_SUCCESS;
+
 }
