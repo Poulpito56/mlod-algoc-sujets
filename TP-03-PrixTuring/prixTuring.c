e7cb87b9a04ee112033fc77ec2261de92b3ac229
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-09-27 12:07:34 +0200

    add fin du TP

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -39,23 +39,145 @@ int numberOfWinners(FILE* f){
 
 char *readStringFromFileUntil(FILE *f, char c){
 	int caractere;
-	char *result = (char*) malloc(512*sizeof(char));
+	char *result = (char*) calloc(512, sizeof(char)); //On prend la place pour 512 char maximum
 	int i = 0;
-	while ((caractere = fgetc(f)) != EOF) {
-    if (caractere==c)
+	int j = -1; //indice décalé car la chaine ce char peut commencer par un ;
+	while ((caractere = fgetc(f)) != EOF && i<511) {
+    if (caractere==c && i>0)
 		{
+			result[j]='\0';
 			return result;
 		}
-		result[i]=caractere;
+		if (caractere != c)
+		{
+			if (i==0)
+			{
+				j++;
+			}
+			result[j]=caractere;
+		}
 		i++;
-  }
+		j++;
+  };
+	result[i]='\0';
+	return result;
 };
+
+void supprimer(Ligne *ligne){
+	free(ligne->nom);	
+	free(ligne->travaux);
+	free(ligne);
+}
 
 void readWinners(FILE *f, Ligne *ligne){
 	fscanf(f, "%i", &ligne->annee);
 	ligne->nom=readStringFromFileUntil(f, ';');
-	ligne->travaux=readStringFromFileUntil(f, ';');
+	ligne->travaux=readStringFromFileUntil(f, '\n');
 };
+
+Ligne* returnLigneWinner(FILE *f){
+	Ligne *ligne = (Ligne*) malloc(sizeof(Ligne));
+	readWinners(f, ligne);
+	return ligne;
+}
+
+void afficherTableauChar(char *string){
+	int i=0;
+	while (i<512 && string[i]!='\0')
+	{	
+		printf("%c", string[i]);
+		i++;
+	}
+	printf("\n");
+};
+
+void ecrireLigne(Ligne *ligne, FILE *fichierPourEcrire){
+	fprintf(fichierPourEcrire, "%d;", ligne->annee);
+	fprintf(fichierPourEcrire, "%s;", ligne->nom);
+	fprintf(fichierPourEcrire, "%s\n", ligne->travaux);
+}
+
+int nbWinners(char filename[]){
+	FILE *fichierACopier = fopen(filename,"r");
+	int nbWin = numberOfWinners(fichierACopier);
+	fclose(fichierACopier);
+	return nbWin;
+}
+
+void printWinners(char filename[], char outputFilename[]){
+	Ligne *ligne;
+	int nbWin = nbWinners(filename);
+	FILE *fichierACopier = fopen(filename,"r");
+	FILE *fichierPourEcrire = fopen(outputFilename,"w");
+	for (int i = 0; i < nbWin; i++)
+	{
+		ligne=returnLigneWinner(fichierACopier);
+		ecrireLigne(ligne, fichierPourEcrire);
+		supprimer(ligne);
+	}
+	fclose(fichierACopier);
+	fclose(fichierPourEcrire);
+}
+
+Ligne* searchLineByAnnee(char filename[], int annee){
+	Ligne *ligne;
+	int nbWin = nbWinners(filename);
+	FILE *fichierACopier = fopen(filename,"r");
+	for (int i = 0; i < nbWin; i++)
+	{
+		ligne=returnLigneWinner(fichierACopier);
+		if (ligne->annee==annee)
+		{
+			return ligne;
+		}
+		supprimer(ligne);
+	}
+	fclose(fichierACopier);
+	return ligne;
+}
+
+
+void infoAnnee(char filename[], int annee){
+	Ligne *ligne = searchLineByAnnee(filename, annee);
+	printf("L'annee %i, le(s) gagnant(s) ont été : %s\nNature des travaux : %s\n", annee, ligne->nom, ligne->travaux);
+	supprimer(ligne);
+}
+
+
+int anneeMin(char filename[]){
+	Ligne *ligne;
+	int nbWin = nbWinners(filename);
+	FILE *fichierACopier = fopen(filename,"r");
+	int anneeMin = 3000;
+	for (int i = 0; i < nbWin; i++)
+	{
+		ligne=returnLigneWinner(fichierACopier);
+		if (ligne->annee<anneeMin)
+		{
+			anneeMin=ligne->annee;
+		}
+		supprimer(ligne);
+	}
+	fclose(fichierACopier);
+	return anneeMin;
+}
+
+void sortTuringWinnersByYear(char filename[], char outputFilename[]){
+	Ligne *ligne;
+	int nbWin = nbWinners(filename);
+	int annee = anneeMin(filename);
+	FILE *fichierACopier = fopen(filename,"r");
+	FILE *fichierPourEcrire = fopen(outputFilename,"w");
+	for (int i = 0; i < nbWin; i++)
+	{
+		ligne=searchLineByAnnee(filename, annee);
+		ecrireLigne(ligne, fichierPourEcrire);
+		supprimer(ligne);
+		annee++; //On suppose que un prix est gagné chaque année ici
+	}
+	fclose(fichierACopier);
+	fclose(fichierPourEcrire);
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
@@ -65,15 +187,12 @@ int main(int argc, char** argv)
 {
 	char filename[] = "turingWinners.csv";
 	char outputFilename[] = "out.csv";
-	FILE* f;
-	FILE* of;
-	f = fopen(filename,"r");
-	of = fopen(outputFilename, "a");
+	char sortedFile[] = "sortedWinners.csv";
 	
-	printf("%i", numberOfWinners(f));
-	
-	fclose(f);
-	fclose(f);
+	printWinners(filename, outputFilename);
+	sortTuringWinnersByYear(filename, sortedFile);
+	infoAnnee(filename, 2003);
+
 	return EXIT_SUCCESS;
 
 }
